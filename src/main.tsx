f8baec0922a3a198294199632405ea7bc35cdf75
Markeljan <Markeljan19@gmail.com>
2022-11-11 22:49:18 -0500

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,49 @@
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+
+export const altLayerDevnet = {
+  id: 9990,
+  name: "AltLayer Devnet",
+  network: "altlayer-devnet",
+  nativeCurrency: { name: "ALT", symbol: "ALT", decimals: 18 },
+  rpcUrls: {
+    default: "https://devnet-rpc.altlayer.io/",
+  },
+  blockExplorers: {
+    default: {
+      name: "altlayer explorer",
+      url: "https://devnet-explorer.altlayer.io/",
+    },
+  },
+  iconUrl: "src/assets/altlayer.ico",
+  testnet: true,
+};
+
+const { provider } = configureChains([altLayerDevnet], [publicProvider() as any]);
+
+const { connectors } = getDefaultWallets({
+  appName: "Flash Raffle",
+  chains: [altLayerDevnet],
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <WagmiConfig client={wagmiClient}>
+    <RainbowKitProvider chains={[altLayerDevnet]} coolMode>
+      <App />
+    </RainbowKitProvider>
+  </WagmiConfig>
+);
